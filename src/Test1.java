363dfb3f4322e8f89a13db79be48102743cadb56
Rasmus Andreasen <rasmus-dam@hotmail.com>
2017-05-15 11:07:25 +0200

    First commit

---
+++ src/Test1.java
@@ -0,0 +1,29 @@
+/**
+ * Created by RasmusPC on 15/05/2017.
+ */
+public class Test1 {
+
+    int num = 15;
+    String horse;
+
+    public Test1(int num, String horse) {
+        this.num = num;
+        this.horse = horse;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getHorse() {
+        return horse;
+    }
+
+    public void setHorse(String horse) {
+        this.horse = horse;
+    }
+}
